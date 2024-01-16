9049adc56c69da5ec3f57d01d5b509032f4a8a77
Saranya <saranya.k@loomy.com>
2024-01-16 20:35:02 +1300

    First commit

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use('/api', router);
 
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000")
-})
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running on port", process.env.PORT || 5000);
+});
